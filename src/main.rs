976b4babd1454c2ceabbf9e57c3931779835cc8a
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2014-11-15 08:39:48 -0500

    Added input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate image;
+use image::GenericImage;
+use std::os;
+
 fn main() {
-    println!("Hello, world!")
+    if os::args().len() < 2 {
+        std::io::stderr().write_str(format!("ERROR:\nUsage: {} inputFile.gif\n", os::args()[0]).as_slice());
+        return;
+    }
+
+    
+    let imageFile = &os::args()[1];
+    let image = image::open(&Path::new(imageFile)).unwrap();
+    println!("Hello, world!\n{}", imageFile);
 }
